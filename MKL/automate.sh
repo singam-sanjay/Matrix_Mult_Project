5a1c3527988b92154d88d49f4072a6fa017ed8db
Sanjay <number1@hariram.com>
2015-03-05 15:46:29 +0530

    Completed the reading for MKLMult. Highest recorded Performance is 66 GFlOps

---
+++ MKL/automate.sh
@@ -0,0 +1,19 @@
+./i_prog 1023
+sleep 10
+./i_prog 1024
+sleep 10
+#./i_prog 1025 # Segfault after alloc matrices
+#sleep 10
+
+./i_prog 2047
+sleep 10
+./i_prog 2048
+sleep 10
+
+./i_prog 4096
+sleep 15
+
+./i_prog 8192
+sleep 15
+
+#./i_prog 16384
---
+++ MKL/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+FILE *input=NULL,*output=NULL;
+double *A = NULL,*B = NULL,*C = NULL;
+size_t N;
+
+#include "../HeaderFiles/matrix_all_func_1DMat.h"
+
+int main(int argc, char** argv)
+{
+	if( argc!=2 )
+	{
+		fprintf(stderr,"Usage:./main N\n");
+		return 11;
+	}
+
+	N = strtoull(argv[1],NULL,10);
+//	N = 3;
+
+	char input_file[50],output_file[20];
+
+	sprintf(input_file,"../OctaveInputs/%zu/input%zu.bin",N,N);
+	input = fopen(input_file,"rb");  // Although 'b' in ignored,
+
+	if( input==NULL )
+	{
+		fprintf(stderr,"The input file \"input%zu.bin\" does not exist.\n",N);
+		return 11;
+	}
+
+	sprintf(output_file,"output%zu.bin",N);
+	output = fopen(output_file,"wb");// Adding it to remind ourselves that these are binary files
+
+	fprintf(stderr,"Allocating 3 %zux%zu matrices\n",N,N);
+
+	MKLAllocA();
+	MKLAllocB();
+	MKLAllocC();
+
+	GetA();
+	GetBSimple();
+
+	MKLMultAB();
+
+	PutC();
+
+	MKLFreeAll();
+
+	fclose(input);
+	fclose(output);
+
+	return 0;
+}
